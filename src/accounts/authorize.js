5584c8e46481a9ab8dcb92574fb2cb847b7e7f73
Varun Barad <varun@varunbarad.com>
2023-04-12 23:54:26 +0530

    Check against stored password

--- src/accounts/authorize.js
+++ src/accounts/authorize.js
@@ -1,1 +1,16 @@
-export async function authorizeUser(email, password) {}
+import bcrypt from 'bcryptjs';
+const { compare } = bcrypt;
+
+export async function authorizeUser(email, password) {
+	const { user } = await import('../user/user.js');
+
+	const userData = await user.findOne({ 'email.address': email });
+	console.log(userData);
+
+	const savedPassword = userData.password;
+
+	const isAuthorized = await compare(password, savedPassword);
+	console.log('isAuthorized', isAuthorized);
+
+	return isAuthorized;
+}
